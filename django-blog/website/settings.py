cb388f5beb1a899bd9d74986104ee56a0a238e9c
Tommy Smith <tommy@tommyesmith.com>
2022-07-03 14:02:51 -0400

    Setup for ngnix proxy

--- django-blog/website/settings.py
+++ django-blog/website/settings.py
@@ -116,6 +116,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_URL = 'static/'
 
